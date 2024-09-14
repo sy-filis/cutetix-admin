cfe8b3ebf9750b88afa14785e247de458295b4b8
Lukáš Matuška <matuska.lukas@lukasmatuska.cz>
2024-09-14 21:46:50 +0000

    Fix angular environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,8 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false,
+  production: true,
   backend: {
-    // api: "https://api-dev.cutetix.rudickamladez.cz",
-    api: "http://localhost:8000",
-    // socketio: "https://api-dev.cutetix.rudickamladez.cz/ws"
+    api: "https://api.cutetix.com",
+    // socketio: "https://api.cutetix.rudickamladez.cz/ws"
   },
   keycloak: {
     // TODO: Update realm
@@ -21,12 +16,3 @@ export const environment = {
     disableAtHashCheck: true
   }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
