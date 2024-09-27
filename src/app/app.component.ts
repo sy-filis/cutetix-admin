6480975a0676502db398e56fa2dfd3c7d0637135
Lukáš Matuška <matuska.lukas@lukasmatuska.cz>
2024-09-27 11:19:49 +0000

    Add DEV to app title on DevMode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, isDevMode } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'cutetix-admin';
+
+  constructor(
+    private titleService: Title
+  ) { }
+  ngOnInit(): void {
+    if (isDevMode()) {
+      this.titleService.setTitle(`DEV ${this.titleService.getTitle()}`)
+    }
+  }
 }
